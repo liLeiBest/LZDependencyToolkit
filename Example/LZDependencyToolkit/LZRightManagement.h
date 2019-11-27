2b102900fb07333d8965f14334be88d27b972428
lilei <lilei_hapy@163.com>
2019-11-27 10:12:32 +0800

    Update，Demo to Test

---
+++ Example/LZDependencyToolkit/LZRightManagement.h
@@ -0,0 +1,18 @@
+//
+//  LZRightManagement.h
+//  LZDependencyToolkit_Example
+//
+//  Created by Dear.Q on 2019/11/26.
+//  Copyright © 2019 lilei_hapy@163.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZRightManagement : NSObject
+
+LZSingletonInterface(LZRightManagement)
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/LZDependencyToolkit/LZRightManagement.m
@@ -0,0 +1,14 @@
+//
+//  LZRightManagement.m
+//  LZDependencyToolkit_Example
+//
+//  Created by Dear.Q on 2019/11/26.
+//  Copyright © 2019 lilei_hapy@163.com. All rights reserved.
+//
+
+#import "LZRightManagement.h"
+
+@implementation LZRightManagement
+
+LZSingletonImplementation(LZRightManagement)
+@end
