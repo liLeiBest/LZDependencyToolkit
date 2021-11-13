92e6dcb6ac6304796236e6c6c3c4caf8891f8bba
lilei <lilei_hapy@163.com>
2021-11-13 16:08:09 +0800

    Update，屏幕尺寸 不能 once

--- LZDependencyToolkit/Classes/Struct/DeviceUnit/LZDeviceUnit.m
+++ LZDependencyToolkit/Classes/Struct/DeviceUnit/LZDeviceUnit.m
@@ -942,16 +942,12 @@ NSString * _language_short_name(void) {
 
 CGSize _screen_size(void) {
     
-    static CGSize size;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        size = [UIScreen mainScreen].bounds.size;
-        if (size.height < size.width) {
-            CGFloat tmp = size.height;
-            size.height = size.width;
-            size.width = tmp;
-        }
-    });
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    if (size.height < size.width) {
+        CGFloat tmp = size.height;
+        size.height = size.width;
+        size.width = tmp;
+    }
     return size;
 }
 
